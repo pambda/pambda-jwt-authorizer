0be9c9c5b3c64565e3c9774ca22fe950aab4d133
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2019-03-30 18:40:29 +0900

    Use lambda-callbackify

--- test/test.js
+++ test/test.js
@@ -23,3 +23,33 @@ test('test', t => {
     t.equal(result.statusCode, 401);
   });
 });
+
+test('test options.unauthorized', t => {
+  t.plan(3);
+
+  const pambda = jwtAuthorizer({
+    unauthorized: async (event, context) => {
+      return {
+        statusCode: 401,
+        body: 'test',
+      };
+    },
+  });
+
+  const lambda = pambda((event, context, callback) => {
+    callback(null, {
+      statusCode: 200,
+      body: 'authorized',
+    });
+  });
+
+  lambda({
+    path: '/',
+    headers: {
+    },
+  }, {}, (err, result) => {
+    t.error(err);
+    t.equal(result.statusCode, 401);
+    t.equal(result.body, 'test');
+  });
+});
